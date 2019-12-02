69cecf5bf56aae8b28a94610f5adec28a5fd835c
arielchm <arielchm@users.noreply.github.com>
2019-12-01 22:16:50 -0400

    Fixed bug in djikstra's algorithm

--- dijkstra.py
+++ dijkstra.py
@@ -9,28 +9,27 @@ adjacency_matrix = [[0, 4, 0, 0, 0, 0, 0, 8, 0],
                     [8, 11, 0, 0, 0, 0, 1, 0, 7],
                     [0, 0, 2, 0, 0, 0, 6, 7, 0]]
 
-size = len(adjacency_matrix)
+length = len(adjacency_matrix)
 
 
 def dijkstra(adjacency_matrix, s):
     visited = []
-    length = len(adjacency_matrix)
     min_distances = [inf]*length
     min_distances[s] = 0
+
     while len(visited) < length:
-        l = []  # lista de distancias a todos los nodos no visitados
+        l = []
         for i in range(length):
             if i not in visited:
                 l.append(min_distances[i])
-                # print(str(l))
         smallest = min(l)
-        index = min_distances.index(smallest)
-        visited.append(index)
+        smallest_index = min_distances.index(smallest)
+        visited.append(smallest_index)
         index = 0
-        for j in adjacency_matrix[index]:
+        for j in adjacency_matrix[smallest_index]:
             if j != 0:
-                alt = min_distances[index] + \
-                    adjacency_matrix[index][index]
+                alt = min_distances[smallest_index] + \
+                    adjacency_matrix[smallest_index][index]
                 if alt < min_distances[index]:
                     min_distances[index] = alt
             index += 1
